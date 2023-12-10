b37cd59e96592e943c7b811611b542ffb280490c
Heneros <punk60826@gmail.com>
2023-12-10 13:17:45 +0200

    first commit

---
+++ templates/contact-us.php
@@ -0,0 +1,10 @@
+<?php
+/*
+* Template Name: Contact Us
+*
+*/
+
+get_header();
+the_title();
+
+get_footer();
---
+++ templates/my-account.php
@@ -0,0 +1,12 @@
+<?php
+/*
+* Template Name: My Account
+*
+*/
+
+get_header ();
+if (have_posts ()){
+while (have_posts ()){the_post (); the_content ();}
+}
+
+get_footer ();
